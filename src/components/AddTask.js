8dc0998e33e2a4ae13e8763d40f871d367d2af68
wissem <wissemanaya1@gmail.com>
2022-03-06 12:37:17 +0100

    finish project

---
+++ src/components/AddTask.js
@@ -0,0 +1,40 @@
+import {useState} from 'react'
+const AddTask = ({onAdd}) => {
+    const [text, setText]=useState('')
+    const [day, setDay]=useState('')
+    const [reminder, setReminder]=useState(false)
+
+    const onSubmit = (e) => {
+      e.preventDefault()
+  
+      if (!text) {
+        alert('Please add a task')
+        return
+      }
+      onAdd({ text, day, reminder })
+
+      setText('')
+      setDay('')
+      setReminder(false)
+    }
+  return (
+    <form className="add-form" onSubmit={onSubmit}>
+      <div className="form-control">
+        <label>Task</label>
+        <input type="text" placeholder="AddTask" value={text} onChange={(e) => setText(e.target.value)}  />
+      </div>
+      <div className="form-control">
+        <label>DayAndTime</label>
+        <input type="text" placeholder="AddDayAndtime" value={day} onChange={(e) => setDay(e.target.value)}/>
+      </div>
+      <div className="form-control form-control-check" >
+        <label>SetReminder</label>
+        <input type="checkbox" checked={reminder} value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)}/>
+      </div>
+      <input type='submit' value='saveTask'
+      className="btn btn-block"/>
+    </form>
+  );
+}
+
+export default AddTask;
---
+++ src/components/Task.js
@@ -0,0 +1,20 @@
+import { FaTimes } from "react-icons/fa";
+function Task({ task, onDelete, onToggle }) {
+  return (
+    <div
+      className={`task ${task.reminder && "reminder"}`}
+      onDoubleClick={() => onToggle(task.id)}
+    >
+      <h3>
+        {task.text}{" "}
+        <FaTimes
+          style={{ color: "red", cursor: "ponter" }}
+          onClick={() => onDelete(task.id)}
+        />
+      </h3>
+      <p>{task.day}</p>
+    </div>
+  );
+}
+
+export default Task;
